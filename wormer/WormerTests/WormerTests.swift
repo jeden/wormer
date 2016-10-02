5b18f846d6cb4738d1a13091a76d40cf11451166
Antonio Bello <jeden@elapsus.com>
2016-10-02 13:30:40 +0200

    Swift 3 migration

--- wormer/WormerTests/WormerTests.swift
+++ wormer/WormerTests/WormerTests.swift
@@ -10,11 +10,9 @@ import XCTest
 import Wormer
 
 private protocol IProtocol {
-	
 }
 
 private class Implementation: IProtocol {
-	
 }
 
 private class UnboundImplementation {
@@ -23,7 +21,7 @@ private class UnboundImplementation {
 
 class WormerTests: XCTestCase {
 	
-	private var injector = Injector.instance
+	fileprivate var injector = Injector.default
 	
 	override func setUp() {
 		super.setUp()
@@ -33,65 +31,55 @@ class WormerTests: XCTestCase {
 	override func tearDown() {
 		super.tearDown()
 	}
-	
-	/**
-	Bind a protocol to an implementation
-	Retrieve an instance and verifies it can be cast to the implementation type
-	*/
+
+	/// Bind a protocol to an implementation
+	/// Retrieve an instance and verifies it can be cast to the implementation type
 	func testObtainInstance() {
-		self.injector.bindInterface(IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
+		self.injector.bind(interface: IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
 		
-		let instance: IProtocol = self.injector.instanceForType(IProtocol.self)
+		let instance: IProtocol = self.injector.instance(for: IProtocol.self)
 		
 		XCTAssertTrue(instance is Implementation, "Instance not of the expected type")
 	}
 	
-	/**
-	Bind a protocol to an implementation
-	Using the overload using type inference to determine the interface type, rather than passing it as parameter
-	*/
+	/// Bind a protocol to an implementation
+	/// Using type inference to determine the interface type, rather than passing it as parameter
 	func testObtainInstanceUsingAltMethod() {
-		self.injector.bindInterface(IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
+		self.injector.bind(interface: IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
 		
-		let instance: IProtocol = self.injector.instanceForType()
+		let instance: IProtocol = self.injector.instance()
 		
 		XCTAssertTrue(instance is Implementation, "Instance not of the expected type")
 	}
 	
-	/**
-	Bind a protocol to an implementation not adopting the protocol itself
-	Verify that retrieving an instance returns nil
-	*/
-	func testRegisterUnboundInstance() {
-		self.injector.bindInterface(IProtocol.self, toImplementation: UnboundImplementation.self, asSingleton: false, initializer: { UnboundImplementation() })
+	/// Bind a protocol to an implementation not adopting the protocol itself
+	/// Verify that retrieving an instance returns nil
+	func testSafeRegisterUnboundInstance() {
+		self.injector.bind(interface: IProtocol.self, toImplementation: UnboundImplementation.self, asSingleton: false, initializer: { UnboundImplementation() })
 		
-		let instance: IProtocol? = self.injector.safeInstanceForType(IProtocol.self)
+		let instance: IProtocol? = self.injector.safeInstance(for: IProtocol.self)
 		
 		XCTAssertTrue(instance == nil, "The instance should be nil")
 	}
-	
-	/**
-	Bind a protocol to an implementation NOT using the singleton mode
-	Verify that a new instance is returned
-	*/
+
+	/// Bind a protocol to an implementation NOT using the singleton mode
+	/// Verify that a new instance is always returned
 	func testNonSingletonReturnDifferentInstances() {
-		self.injector.bindInterface(IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
+		self.injector.bind(interface: IProtocol.self, toImplementation: Implementation.self, asSingleton: false, initializer: { Implementation() })
 		
-		let instance1 = self.injector.instanceForType(IProtocol.self) as! Implementation
-		let instance2 = self.injector.instanceForType(IProtocol.self) as! Implementation
+		let instance1 = self.injector.instance(for: IProtocol.self) as! Implementation
+		let instance2 = self.injector.instance(for: IProtocol.self) as! Implementation
 		
 		XCTAssertTrue(instance1 !== instance2, "The 2 instances should be different")
 	}
 	
-	/**
-	Bind a protocol to an implementation using the singleton mode
-	Verify that the same instance is returned
-	*/
+	/// Bind a protocol to an implementation using the singleton mode
+	/// Verify that the same instance is returned
 	func testSingletonReturnDifferentInstances() {
-		self.injector.bindInterface(IProtocol.self, toImplementation: Implementation.self, asSingleton: true, initializer: { Implementation() })
+		self.injector.bind(interface: IProtocol.self, toImplementation: Implementation.self, asSingleton: true, initializer: { Implementation() })
 		
-		let instance1 = self.injector.instanceForType(IProtocol.self) as! Implementation
-		let instance2 = self.injector.instanceForType(IProtocol.self) as! Implementation
+		let instance1 = self.injector.instance(for: IProtocol.self) as! Implementation
+		let instance2 = self.injector.instance(for: IProtocol.self) as! Implementation
 		
 		XCTAssertTrue(instance1 === instance2, "The 2 references should point to the same instance")
 	}
